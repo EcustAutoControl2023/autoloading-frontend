3645ad06a435ebc985bef8ea4ea40a8f2de58258
Chen Jiyang <masarity@outlook.com>
2023-08-19 22:46:13 +0800

    🎉 init(整个项目): 上传当前代码

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+function show()
+{
+    let show = $(".dialog").css("display");
+    $(".dialog").css("display", show=="none"?"block":"none");
+}
+function close()
+{
+    let show = $(".dialog").css("display");
+    $(".dialog").css("display", show=="none"?"block":"none");
+}
